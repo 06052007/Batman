ef6ca7d7ab79ac427d87f8cf10bd5b564dff174c
06052007 <70326795+06052007@users.noreply.github.com>
2021-01-04 17:36:46 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+
+function preload(){
+    
+}
+
+function setup(){
+  var maxDrops = 100;
+  for( i=0 i<maxDrops; i++){
+      drops.push(new createDrop(random(0,400), random(0,400)))
+      friction = 0,1
+      if(this.position.y>
+  } 
+    
+}
+
+function draw(){
+    
+}   
+
